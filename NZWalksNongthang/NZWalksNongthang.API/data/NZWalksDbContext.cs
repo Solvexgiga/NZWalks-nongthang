7f66cce99dadb8eb056162fe5f16b79dad19b657
Solvexgiga <solvexgiga@gmail.com>
2022-11-09 16:59:12 +0530

    end of section 2

---
+++ NZWalksNongthang/NZWalksNongthang.API/data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksNongthang.API.Model.Domain;
+namespace NZWalksNongthang.API.data
+
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
